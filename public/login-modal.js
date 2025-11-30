4a2290873e8ccc283e66530c7e44dcec88bfdab0
trucdaobodycare <trucdaobodycare@gmail.com>
2025-11-30 15:11:54 +0700

    Add login modal script

--- public/login-modal.js
+++ public/login-modal.js
@@ -1,1 +1,10 @@
-fix: add login-modal.js and include script to open/close login modal
+// JavaScript code for login modal
+function openLoginModal() {
+    // Logic to open the modal
+}
+
+function closeLoginModal() {
+    // Logic to close the modal
+}
+
+// Other modal functionality
